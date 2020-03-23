8ff02c103600c6c7c6aedc92fb4d96d5322d05aa
Cotet Roxana <cotetroxana@gmail.com>
2020-03-23 17:50:26 +0200

    Laborator 4

---
+++ lab4/ModelDesignFirst_L1/ModelDesignFirst_L1/ModelOneToManyContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDesignFirst_L1
+{
+    public partial class ModelOneToManyContainer : DbContext
+    {
+        public ModelOneToManyContainer()
+        : base("name=ModelOneToManyContainer")
+        { }
+
+        protected override void OnModelCreating(
+       DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+
+}
