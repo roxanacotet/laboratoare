8201093bb6c1be614609827d369a76dfec324c68
Cotet Roxana <cotetroxana@gmail.com>
2020-05-05 13:40:22 +0300

    Add files via upload

---
+++ lab10/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Asp_Cotet_Andreea_Roxana_Rp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Cotet_Andreea_Roxana_Rp.Models
+{
+	public class SeedData
+	{
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Asp_Cotet_Andreea_Roxana_RpContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<Asp_Cotet_Andreea_Roxana_RpContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
