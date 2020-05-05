1436bf7a2e6c01c57e7cfcd1746bb5fb9133ac4f
Cotet Roxana <cotetroxana@gmail.com>
2020-05-05 13:38:19 +0300

    Add files via upload

---
+++ Data/Asp_Cotet_Andreea_Roxana_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Cotet_Andreea_Roxana_Rp.Models;
+
+namespace Asp_Cotet_Andreea_Roxana_Rp.Data
+{
+    public class Asp_Cotet_Andreea_Roxana_RpContext : DbContext
+    {
+        public Asp_Cotet_Andreea_Roxana_RpContext (DbContextOptions<Asp_Cotet_Andreea_Roxana_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Cotet_Andreea_Roxana_Rp.Models.Movie> Movie { get; set; }
+    }
+}
