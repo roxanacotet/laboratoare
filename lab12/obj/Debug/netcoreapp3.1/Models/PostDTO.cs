1a8b3ec0863d78e6d6df22c4900321cd32c5b867
Cotet Roxana <cotetroxana@gmail.com>
2020-05-13 17:42:16 +0300

    Add files via upload

---
+++ lab12/obj/Debug/netcoreapp3.1/Models/PostDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovies.Models
+{
+    [DataContract]
+    public class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new List<CommentDTO>(); //new HashSet<Comment>();
+        }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Domain { get; set; }
+        [DataMember]
+        public System.DateTime Date { get; set; }
+        [DataMember]
+        public virtual List<CommentDTO> Comments { get; set; }
+    }
+}
---
+++ lab12/obj/Debug/netcoreapp3.1/Models/commentDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovies.Models
+{
+    [DataContract(IsReference = true)]
+    public class CommentDTO
+	{
+        [DataMember]
+        public int CommentId { get; set; }
+        [DataMember]
+        public string Text { get; set; }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public int PostPostId { get; set; }
+        [DataMember]
+        public virtual PostDTO Post { get; set; }
+    }
+}
